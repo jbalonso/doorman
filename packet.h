472e6b348b961e752da8b260d339426d1f7028e3
Jason Bernardino Alonso <jalonso@media.mit.edu>
2011-03-26 05:40:26 -0400

    Cutting back on the maximum argument length

--- packet.h
+++ packet.h
@@ -7,7 +7,7 @@
 const int signature_len = 20;
 const int session_secret_len = signature_len;
 const int max_cmd_len = 8;
-const int max_args_len = 32;
+const int max_args_len = 16;
 const unsigned long max_signature_age = 500;            // 500 ms
 const unsigned long max_key_age = (10L*60L*60L*1000L);  // 10 hours
 
